3713b67b59757851ed91ba616005d6122b20ef0a
directoryx <anggawijaya4567@gmail.com>
2019-07-21 20:48:05 +0700

    success auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'home/index'
+  root 'home#index'
+  resources :sessions
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
